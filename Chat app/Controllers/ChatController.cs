cde45af62a5aafdc890e1c0208910b1a55172bf4
carrotunderscore <robertpergustafsson@gmail.com>
2024-08-08 13:22:15 +0200

    first commit

---
+++ Chat app/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chat_app.Controllers
+{
+	[Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase 
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Chat app/entity framework/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Chat_app.entity_framework
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+		{
+		}
+
+		public DbSet<Message> Messages { get; set; }
+	}
+}
